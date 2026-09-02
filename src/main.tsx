31b80d1abb64a5b71ee3e4af152dac5c447d75d6
berkozturkawsdev@gmail.com <berkozturkawsdev@gmail.com>
2026-09-02 12:13:24 +0000

    remove bg

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-import background from "./assets/bg.jpg";
-
-document.body.style.backgroundImage = `url(${background})`;
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
 
